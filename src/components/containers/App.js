b2f65704145852e11ed35a5f26553910e3042e45
gregorsunta <gresunta@gmail.com>
2023-01-25 11:33:35 +0100

    add initial file structure and install styled components

---
+++ src/components/containers/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
+import Home from '../homepage/Home';
+import Shop from '../homepage/Shop';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path={'/'} element={<Home />} />
+        <Route path={'/shop'} element={<Shop />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
